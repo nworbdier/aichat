10fd504374607e0bfeada281ba8538e5fa28dcbf
Reid Brown <reidbrown23@gmail.com>
2024-04-20 11:21:53 -0400

    Updates

--- navigation/index.tsx
+++ navigation/index.tsx
@@ -1,5 +1,8 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { View, SafeAreaView, StyleSheet, Platform } from 'react-native';
 
 import { BackButton } from '../components/BackButton';
 import Details from '../screens/details';
@@ -14,21 +17,45 @@ const Stack = createStackNavigator<RootStackParamList>();
 
 export default function RootStack() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Overview">
-        <Stack.Screen
-          name="Overview"
-          component={ChatScreen}
-          options={{ headerShown: false }} // Set headerShown to false
-        />
-        <Stack.Screen
-          name="Details"
-          component={Details}
-          options={({ navigation }) => ({
-            headerLeft: () => <BackButton onPress={navigation.goBack} />,
-          })}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <View style={styles.app}>
+      <StatusBar style="auto" />
+      {Platform.OS !== 'ios' && <View style={styles.customTopView} />}
+      <SafeAreaView style={styles.appTop} />
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Overview">
+          <Stack.Screen
+            name="Overview"
+            component={ChatScreen}
+            options={{ headerShown: false }} // Set headerShown to false
+          />
+          <Stack.Screen
+            name="Details"
+            component={Details}
+            options={({ navigation }) => ({
+              headerLeft: () => <BackButton onPress={navigation.goBack} />,
+            })}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  app: {
+    flex: 1,
+    backgroundColor: 'white',
+  },
+  appTop: {
+    backgroundColor: '#E5E5EA',
+  },
+  customTopView: {
+    width: '100%',
+    height: '5%',
+    backgroundColor: '#E5E5EA',
+  },
+  loadingScreen: {
+    flex: 1,
+    backgroundColor: 'black',
+  },
+});
--- screens/overview.tsx
+++ screens/overview.tsx
@@ -1,3 +1,4 @@
+import { Ionicons } from '@expo/vector-icons';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import React, { useState, useRef, useEffect } from 'react';
 import {
@@ -17,6 +18,7 @@ const ChatScreen = () => {
   const [userInput, setUserInput] = useState('');
   const flatListRef = useRef(null);
   const [modalVisible, setModalVisible] = useState(false); // State to control modal visibility
+  const [chatHistory, setChatHistory] = useState([]); // State to store the entire chat history
 
   // Initialize instances with API key
   const openRouterKey = process.env.EXPO_PUBLIC_OPEN_ROUTER_API_KEY;
@@ -27,7 +29,7 @@ const ChatScreen = () => {
     Llama: 'meta-llama/llama-3-8b-instruct:nitro',
   };
 
-  const [selectedModel, setSelectedModel] = useState('ChatGPT'); // State to track the selected model
+  const [selectedModel, setSelectedModel] = useState('Llama'); // State to track the selected model
 
   // Function to load messages from AsyncStorage when component mounts
   const loadMessages = async () => {
@@ -58,6 +60,19 @@ const ChatScreen = () => {
   const handleChat = async () => {
     try {
       const model = models[selectedModel]; // Get the model based on selectedModel
+
+      // Create a new message object for the user's input
+      const newUserMessage = {
+        role: 'user',
+        content: userInput,
+      };
+
+      // Append the new message to the message list
+      setMessages((prevMessages) => [...prevMessages, { user: true, content: userInput }]);
+
+      // Clear the text input immediately
+      setUserInput('');
+
       const url = 'https://openrouter.ai/api/v1/chat/completions';
 
       const response = await fetch(url, {
@@ -68,7 +83,7 @@ const ChatScreen = () => {
         },
         body: JSON.stringify({
           model,
-          messages: [{ role: 'user', content: userInput }],
+          messages: [...chatHistory, newUserMessage], // Include the entire chat history
         }),
       });
 
@@ -79,24 +94,22 @@ const ChatScreen = () => {
 
       const responseData = await response.json();
 
-      const newMessage = {
-        user: true,
-        content: userInput,
-      };
-
-      const responseMessage = {
+      // Create a new message object for the response
+      const newResponseMessage = {
         user: false,
         content: responseData.choices[0].message.content.trim(), // Trim leading whitespace
       };
 
-      const updatedMessages = [...messages, newMessage, responseMessage];
-      setMessages(updatedMessages);
-      setUserInput('');
+      // Append the response message to the message list
+      setMessages((prevMessages) => [...prevMessages, newResponseMessage]);
 
       // Save messages to AsyncStorage
-      saveMessages(updatedMessages);
+      saveMessages([...messages, newUserMessage, newResponseMessage]);
 
-      // Scroll to the end of the list
+      // Update chat history
+      setChatHistory([...chatHistory, newUserMessage]);
+
+      // Scroll to the end of the list after the response message is added
       flatListRef.current.scrollToEnd({ animated: true });
     } catch (error) {
       console.error('Error:', error);
@@ -107,6 +120,8 @@ const ChatScreen = () => {
     // Clear messages from state and AsyncStorage
     setMessages([]);
     AsyncStorage.removeItem('messages');
+    // Clear chat history
+    setChatHistory([]);
   };
 
   const renderItem = ({ item }) => (
@@ -122,15 +137,17 @@ const ChatScreen = () => {
         <View style={styles.pickerContainer}>
           <Text style={styles.selectedModel}>{selectedModel}</Text>
           <TouchableOpacity onPress={() => setModalVisible(true)} style={styles.pickerButton}>
-            <Text style={{ fontSize: 16 }}>Model ▼</Text>
+            <Text style={{ fontSize: 16, fontWeight: 'bold' }}>Model ▼</Text>
           </TouchableOpacity>
         </View>
         <FlatList
           ref={flatListRef}
           data={messages}
           renderItem={renderItem}
+          style={styles.flatlist}
           keyExtractor={(item, index) => index.toString()}
         />
+        <View style={styles.divider} />
         <View style={styles.inputContainer}>
           <TextInput
             style={styles.input}
@@ -150,6 +167,12 @@ const ChatScreen = () => {
         visible={modalVisible}
         onRequestClose={() => setModalVisible(false)}>
         <View style={styles.modalContainer}>
+          <View style={styles.filterSheetHeader}>
+            <Text style={styles.filterSheetTitle}>Models:</Text>
+            <TouchableOpacity onPress={() => setModalVisible(false)}>
+              <Ionicons name="close-circle-outline" size={35} color="black" />
+            </TouchableOpacity>
+          </View>
           <View style={styles.modalContent}>
             {Object.keys(models).map((model) => (
               <TouchableOpacity
@@ -171,34 +194,50 @@ const ChatScreen = () => {
 const styles = StyleSheet.create({
   content: {
     flex: 1,
-    paddingTop: 70,
     paddingBottom: 30,
-    paddingHorizontal: 20,
+    backgroundColor: '#E5E5EA',
   },
+  flatlist: { marginHorizontal: 20 },
   pickerContainer: {
     flexDirection: 'row',
     justifyContent: 'space-between',
     alignItems: 'center',
     marginBottom: 10,
+    paddingHorizontal: 20,
   },
   selectedModel: {
     fontWeight: 'bold',
     fontSize: 25,
   },
   pickerButton: {
-    paddingHorizontal: 20,
-    paddingVertical: 10,
+    paddingHorizontal: 15,
+    paddingVertical: 8,
     backgroundColor: '#E5E5EA',
     borderRadius: 5,
+    borderColor: 'black',
+    borderWidth: 0.5,
   },
   modalContainer: {
     position: 'absolute',
+    flexDirection: 'column',
     bottom: 0,
     height: '50%',
     width: '100%',
-    backgroundColor: '#FFFFFF',
+    backgroundColor: 'white',
     borderTopLeftRadius: 20,
     borderTopRightRadius: 20,
+  },
+  filterSheetHeader: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginRight: 20,
+    marginTop: 20,
+  },
+  filterSheetTitle: {
+    fontSize: 25,
+    fontWeight: 'bold',
+    marginLeft: 20,
   },
   modalContent: {
     padding: 20,
@@ -206,7 +245,6 @@ const styles = StyleSheet.create({
   modelOption: {
     fontSize: 20,
     paddingVertical: 10,
-    fontWeight: 'bold',
   },
   messageContainer: {
     padding: 10,
@@ -220,7 +258,7 @@ const styles = StyleSheet.create({
   },
   responseMessage: {
     alignSelf: 'flex-start',
-    backgroundColor: '#E5E5EA',
+    backgroundColor: 'white',
   },
   messageText: {
     fontSize: 16,
@@ -229,6 +267,13 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     alignItems: 'center',
     padding: 5,
+    paddingHorizontal: 20,
+  },
+  divider: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    width: '100%',
+    paddingVertical: 5,
     borderTopWidth: 1,
     borderTopColor: '#CCCCCC',
   },
